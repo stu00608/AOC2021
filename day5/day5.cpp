1400db213b0c5d3f7651584b57b42d3913c3b6e7
Allen Shen <shen.allen@inventec.com>
2022-03-19 00:01:17 +0800

    Completed day5.

--- day5/day5.cpp
+++ day5/day5.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
+#include <cstring>
 #include <fstream>
+#include <iostream>
+#include <set>
 #include <sstream>
 #include <vector>
-#include <cstring>
 
 const bool DEBUG = false;
 
@@ -10,22 +11,16 @@ typedef std::pair<int, int> Point;
 typedef std::pair<Point, Point> Line;
 typedef std::vector<std::vector<int>> Table;
 
-void showLine(Line line)
-{
-    std::printf("p1 = (%d, %d), p2 = (%d, %d)\n", line.first.first, line.first.second, line.second.first, line.second.second);
+void showLine(Line line) {
+    std::printf("p1 = (%d, %d), p2 = (%d, %d)\n", line.first.first,
+                line.first.second, line.second.first, line.second.second);
 }
 
-void showPoint(Point pt)
-{
-    std::printf("(%d, %d))\n", pt.first, pt.second);
-}
+void showPoint(Point pt) { std::printf("(%d, %d))\n", pt.first, pt.second); }
 
-void showTable(Table &table)
-{
-    for (size_t i = 0; i < table.size(); i++)
-    {
-        for (size_t j = 0; j < table[i].size(); j++)
-        {
+void showTable(Table &table) {
+    for (size_t i = 0; i < table.size(); i++) {
+        for (size_t j = 0; j < table[i].size(); j++) {
             std::cout << table[i][j];
         }
 
@@ -33,14 +28,11 @@ void showTable(Table &table)
     }
 }
 
-void dumpTable(Table &table, std::string filename)
-{
+void dumpTable(Table &table, std::string filename) {
     std::ofstream outFile;
     outFile.open(filename);
-    for (size_t i = 0; i < table.size(); i++)
-    {
-        for (size_t j = 0; j < table[i].size(); j++)
-        {
+    for (size_t i = 0; i < table.size(); i++) {
+        for (size_t j = 0; j < table[i].size(); j++) {
             outFile << table[i][j] << " ";
         }
         outFile << std::endl;
@@ -48,22 +40,18 @@ void dumpTable(Table &table, std::string filename)
     outFile.close();
 }
 
-std::vector<Line> getInputData(std::ifstream &file)
-{
+std::vector<Line> getInputData(std::ifstream &file) {
     std::string inputLine;
     std::string substring = " -> ";
     std::vector<Line> result;
- 
-    
-    while(getline(file, inputLine))
-    {
+
+    while (getline(file, inputLine)) {
         inputLine.replace(inputLine.find(substring), substring.length(), ",");
 
         std::istringstream ss(inputLine);
         std::string pointStr;
         std::vector<int> pointSet;
-        while(getline(ss, pointStr, ','))
-        {
+        while (getline(ss, pointStr, ',')) {
             pointSet.push_back(std::stoi(pointStr));
         }
 
@@ -76,99 +64,104 @@ std::vector<Line> getInputData(std::ifstream &file)
         result.push_back(line);
     }
 
-    if(DEBUG)
-    {
-        for (size_t i = 0; i < result.size(); i++)
-        {
-           showLine(result[i]); 
+    if (DEBUG) {
+        for (size_t i = 0; i < result.size(); i++) {
+            showLine(result[i]);
         }
     }
 
     return result;
 }
 
-int getMaxSizeX(std::vector<Line> lineList)
-{
+int getMaxSizeX(std::vector<Line> lineList) {
     int max = 0;
-    for (size_t i = 0; i < lineList.size(); i++)
-    {
+    for (size_t i = 0; i < lineList.size(); i++) {
         max = std::max(max, lineList[i].first.first);
         max = std::max(max, lineList[i].second.first);
     }
     return max;
 }
 
-int getMaxSizeY(std::vector<Line> lineList)
-{
+int getMaxSizeY(std::vector<Line> lineList) {
     int max = 0;
-    for (size_t i = 0; i < lineList.size(); i++)
-    {
+    for (size_t i = 0; i < lineList.size(); i++) {
         max = std::max(max, lineList[i].first.second);
         max = std::max(max, lineList[i].second.second);
     }
     return max;
 }
 
-int8_t directionOfLine(Line line) {
+std::string directionOfLine(Line line) {
     // 1:vertical, -1:horizontal, 0:cross
 
-    if(line.second.first-line.first.first == 0)
-    {
-        return 1;
-    }
-    else if(line.second.second-line.first.second == 0)
-    {
-        return -1;
-    }
-    else
-    {
-        return 0;
-    }
+    // Part2, considering diagonal line (only 45 degree)
+    // 0,0 -> 1,1
+    // 0,1 -> 1,0
+    // 1,0 -> 0,1
+    // 0,1 -> 2,3
+    // 3,2 -> 1,0
 
+    // TODO: Should calculate the slope and return.
+
+    if (std::abs(line.first.first - line.second.first) ==
+        std::abs(line.first.second - line.second.second)) {
+        // 45 deg 2 points.
+        return "diagonal";
+    } else if (line.second.first == line.first.first) {
+        return "vertical";
+    } else if (line.second.second == line.first.second) {
+        return "horizontal";
+    } else {
+        printf("Unknown case : ");
+        showLine(line);
+        return "other";
+    }
 }
 
-std::vector<Point> extractPointFromLine(Line line)
-{
+std::vector<Point> extractPointFromLine(Line line) {
     // 0,9 -> 5,9
     // 8,0 -> 0,8
     // Only consider x1==x2 or y1==y2
     std::vector<Point> result;
 
-    if(directionOfLine(line)>0)
-    {
-        if(line.first.second > line.second.second)
-        {
+    if (directionOfLine(line) == "vertical") {
+        if (line.first.second > line.second.second) {
             std::swap(line.first.second, line.second.second);
         }
 
-        for (size_t i =line.first.second; i <= line.second.second; i++)
-        {
+        for (size_t i = line.first.second; i <= line.second.second; i++) {
             Point pt(line.first.first, i);
             result.push_back(pt);
         }
-    }
-    else if(directionOfLine(line)<0)
-    {
-        if(line.first.first > line.second.first)
-        {
+    } else if (directionOfLine(line) == "horizontal") {
+        if (line.first.first > line.second.first) {
             std::swap(line.first.first, line.second.first);
         }
 
-        for (size_t i =line.first.first; i <= line.second.first; i++)
-        {
+        for (size_t i = line.first.first; i <= line.second.first; i++) {
             Point pt(i, line.first.second);
             result.push_back(pt);
         }
-    }
-    // else
-    // {
-        // std::cout << "Cross line" << std::endl;
-    // }
+    } else if (directionOfLine(line) == "diagonal") {
+        if (line.first.first > line.second.first) {
+            std::swap(line.first, line.second);
+        }
 
-    if(DEBUG)
-    {
-        for (size_t i = 0; i < result.size(); i++)
-        {
+        int x_dir = (line.first.first - line.second.first < 0) ? 1 : -1;
+        int y_dir = (line.first.second - line.second.second < 0) ? 1 : -1;
+
+        for (size_t i = line.first.first, j = line.first.second; true;
+             i += x_dir, j += y_dir) {
+            Point pt(i, j);
+            result.push_back(pt);
+            if (i == line.second.first && j == line.second.second) {
+                break;
+            }
+        }
+    }
+
+    if (DEBUG) {
+        for (size_t i = 0; i < result.size(); i++) {
             showPoint(result[i]);
         }
     }
@@ -176,55 +169,44 @@ std::vector<Point> extractPointFromLine(Line line)
     return result;
 }
 
-void setPointInTable(Table &table, Point point)
-{
+void setPointInTable(Table &table, Point point) {
     table[point.second][point.first] += 1;
 }
 
-void applyLineInTable(Table &table, Line line)
-{
+void applyLineInTable(Table &table, Line line) {
     std::vector<Point> path = extractPointFromLine(line);
-    if (path.empty())
-    {
+    if (path.empty()) {
         return;
     }
-    
-    for (size_t i = 0; i < path.size(); i++)
-    {
+
+    for (size_t i = 0; i < path.size(); i++) {
         setPointInTable(table, path[i]);
     }
 }
 
-
-
-int main()
-{
+int main() {
     std::ifstream inputFile("input");
     std::vector<Line> lineList = getInputData(inputFile);
 
-    int height = getMaxSizeY(lineList)+1;
-    int width = getMaxSizeX(lineList)+1;
+    int height = getMaxSizeY(lineList) + 1;
+    int width = getMaxSizeX(lineList) + 1;
 
     std::vector<std::vector<int>> table(width, std::vector<int>(height));
 
-    for (size_t i = 0; i < lineList.size(); i++)
-    {
+    for (size_t i = 0; i < lineList.size(); i++) {
         applyLineInTable(table, lineList[i]);
     }
 
     int ans = 0;
-    for (size_t i = 0; i < table.size(); i++)
-    {
-        for (size_t j = 0; j < table[i].size(); j++)
-        {
-            if (table[i][j] >= 2)
-            {
+    for (size_t i = 0; i < table.size(); i++) {
+        for (size_t j = 0; j < table[i].size(); j++) {
+            if (table[i][j] >= 2) {
                 ans += 1;
             }
         }
     }
 
     printf("\n\n%d\n", ans);
-    
+
     return 0;
 }
