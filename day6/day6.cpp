739bbcf8ee17e15f10dc7f061cda3a4fb10258ec
Allen Shen <shen.allen@inventec.com>
2022-03-23 13:46:56 +0800

    Part2, refer to Christmann's code.

--- day6/day6.cpp
+++ day6/day6.cpp
@@ -4,40 +4,14 @@
 #include <sstream>
 #include <vector>
 
-class Fish {
-   public:
-    uint8_t value;
-    uint8_t offset;
-
-    Fish(uint8_t a, uint8_t b) {
-        this->value = a;
-        this->offset = b;
+void showFishList(std::vector<uint64_t>& fishList) {
+    for(auto& item: fishList) {
+        std::cout << item << " ";
     }
-};
-
-std::vector<Fish> fishList;
-int days = 256;
-int fishCount = 0;
-
-void fishAlgorithm(Fish fish) {
-    int pos = days - fish.offset;
-    if (pos > fish.value) {
-        int child = (pos - fish.value) / 7 + 1;
-        for (size_t i = 0; i < child; i++) {
-            int newOffset = fish.offset + 7 * i + fish.value + 1;
-            if (newOffset > days) {
-                continue;
-            }
-            // std::cout << newOffset << " ";
-            Fish newFish(8, newOffset);
-            fishList.push_back(newFish);
-        }
-        // std::cout << std::endl;
-    }
-    fishList.erase(fishList.begin());
-    fishCount++;
+    std::cout << std::endl;
 }
 
+// TODO: Clean the code.
 int main(void) {
     std::ifstream inputFile("input");
 
@@ -45,20 +19,37 @@ int main(void) {
     getline(inputFile, inputLine);
     std::istringstream ss(inputLine);
     std::string element;
+    std::vector<uint64_t> fishList(9);
+
+    // 0 1 2 3 4 5 6 7 8
+    //   1 1 2 1 
     while (getline(ss, element, ',')) {
-        Fish fish(std::stoi(element), 0);
-        fishList.push_back(fish);
+        fishList[std::stoi(element)]++;
     }
 
+    int days=256;
+    while (days--)
+    {
+        uint64_t deadFish = fishList[0];
+        fishList[0] = fishList[1];
+        fishList[1] = fishList[2];
+        fishList[2] = fishList[3];
+        fishList[3] = fishList[4];
+        fishList[4] = fishList[5];
+        fishList[5] = fishList[6];
+        fishList[6] = fishList[7] + deadFish;
+        fishList[7] = fishList[8];
+        fishList[8] = deadFish;
+    }
+    
+    showFishList(fishList);
 
-    while(!fishList.empty()) {
-        fishAlgorithm(fishList[0]);        
-        std::cout << fishCount << std::endl;
+    int64_t ans=0;
+    for(auto& fish : fishList) {
+        ans+=fish;
     }
 
-    std::vector<Fish> copy(fishList);
-
-    std::cout << fishCount << std::endl;
+    std::cout << ans << std::endl;
 
     return 0;
 }
